30b50f41382ecd31fbe62768c7069d735777d621
Agung Prasetyo <brain64bit@gmail.com>
2016-09-21 23:47:57 +0700

    setup app

--- config/fulcrum_defaults.rb
+++ config/fulcrum_defaults.rb
@@ -6,10 +6,10 @@ Configuration.for('fulcrum') do
   mailer_sender ENV['MAILER_SENDER'] || 'noreply@example.com'
 
   # Disable registration pages
-  disable_registration ENV['DISABLE_REGISTRATION'] || false
+  disable_registration ENV['DISABLE_REGISTRATION'] || true
 
   # Project column order:
   # progress_to_right: chilly bin, backlog, in progress, done
   # progress_to_left: done, in progress, backlog, chilly bin
-  column_order 'progress_to_left'
+  column_order 'progress_to_right'
 end
